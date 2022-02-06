34f26c2399bd475467ea4211e62b60a2bb412871
chon <balint.ducsai@gmail.com>
2022-02-06 20:22:08 +0100

    add chat screen

---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Heading } from 'native-base';
+
+export default function ChatScreen() {
+  return (
+    <View justifyContent={'center'} alignItems={'center'}>
+      <Heading>Chat Works!</Heading>
+    </View>
+  );
+}
